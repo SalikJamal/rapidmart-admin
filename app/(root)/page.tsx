7a55c69588998881ef7d511864be69c7518d13e3
Salik <iamsalikjamal@gmail.com>
2024-04-22 22:13:21 +0500

    Completed Clerk auth with latest version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
 
 
 export default function SetupPage() {
     return (
         <div className="p-4">
-            This is a protected route!
+            <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
