ac22d563715b858e473523dedb989c609f31413e
Salik <iamsalikjamal@gmail.com>
2024-04-25 11:17:24 +0500

    Dashboard routes

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,33 @@
+import prismadb from "@/lib/prismadb"
+import { IReactChildren } from "@/lib/types"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+interface IDashboardLayoutProps extends IReactChildren {
+    params: {
+        storeId: string
+    }
+}
+
+
+export default async function DashboardLayout({ children, params }: IDashboardLayoutProps) {
+    
+    const { userId } = auth()
+    if(!userId) redirect("/sign-in")
+    
+    const store = await prismadb.store.findFirst({ 
+        where: { 
+            id: params.storeId, 
+            userId 
+        } 
+    })
+
+    if(!store) redirect("/")
+
+    return (
+        <>
+            <div>This will be a Navbar</div>
+            {children}
+        </>
+    )
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb"
+import { IReactChildren } from "@/lib/types"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+
+export default async function SetupLayout({ children }: IReactChildren) {
+
+    const { userId } = auth()
+    if(!userId) redirect("/sign-in")
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            userId
+        }
+    })
+
+    if(store) redirect(`/dashboard/${store.id}`)
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
