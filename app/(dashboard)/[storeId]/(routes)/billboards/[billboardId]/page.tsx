fbe2613c0903538910dac5b41d921635c8be30e7
Salik <iamsalikjamal@gmail.com>
2024-04-29 15:03:18 +0500

    Bilboard form

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,5 +1,26 @@
-export default function BillboardPage() {
+import BillboardForm from "@/components/store/billboards/billboard-form";
+import prismadb from "@/lib/prismadb"
+
+interface IBillboardPageProps {
+    params: {
+        billboardId: string;
+    };
+}
+
+
+export default async function BillboardPage({ params }: IBillboardPageProps) {
+    
+    const billboard = await prismadb.billboard.findUnique({
+        where: {
+            id: params.billboardId
+        }
+    })
+
     return (
-        <div>This is a form for billboards</div>
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardForm initialData={billboard} />
+            </div>
+        </div>
     )
 }
