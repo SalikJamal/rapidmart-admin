1452dbb4eebd5d973bf1367532d7c513ed9a8ec8
Salik <iamsalikjamal@gmail.com>
2024-05-29 00:00:13 +0500

    Theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
-import "./globals.css"
 import ModalProvider from "@/providers/modal-provider"
 import ToastProvider from "@/providers/toast-provider"
+import { ThemeProvider } from "@/providers/theme-provider"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,9 +23,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToastProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
